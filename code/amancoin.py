9d884d1b43cac8de2c3261fb4aea96d5cf77e123
Aman Ali <pamanalionline@gmail.com>
2018-07-15 08:41:42 -0400

    Update amancoin.py

--- code/amancoin.py
+++ code/amancoin.py
@@ -199,6 +199,18 @@ def connect_node():
                    'total_nodes':list(blockchain.nodes)}
       return jsonify(response), 201
 
+# Replacing the chain by the longest chain if needed
+@app.route('/replace_chain',methods=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain() # method returns true/false and replaces the chain variable of the object
+    if is_chain_replaced:
+        response = {'message':'Yes the nodes had different chains, so the chain was replaced by the longest one',
+                    'new_chain':blockchain.chain} # displaying the chain which is updated by the replace chain method
+    else:
+        response = {'message':'All good. The chain is the largest one.',
+                    'actual_chain':blockchain.chain}
+    return jsonify(response), 200
+
 #running the app
 app.run(host = '0.0.0.0', port = 5000 )
 
